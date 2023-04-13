a4e2f05add97de79d952adf5e463f64bd37bea46
rg-ept <130046649+rg-ept@users.noreply.github.com>
2023-04-13 14:28:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 42877418
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
